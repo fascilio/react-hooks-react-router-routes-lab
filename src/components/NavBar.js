b724c862c6f2907ff0ace0a95f2405180f296f76
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-04-27 12:41:09 +0300

    router lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,22 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <NavLink exact to="/" style={{ fontSize: "24px" }}>
+        Home
+      </NavLink>
+      <NavLink exact to="/movies" style={{ fontSize: "24px" }}>
+        Movies
+      </NavLink>
+      <NavLink exact to="/directors" style={{ fontSize: "24px" }}>
+        Directors
+      </NavLink>
+      <NavLink exact to="/actors" style={{ fontSize: "24px" }}>
+        Actors
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
